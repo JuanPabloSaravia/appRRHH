ed62dff01c2a191fa59114867b4adf06b12e1315
--get <--get>
2025-10-04 06:47:11 -0300

    Se cambio el front desde reac a react + VITE

--- rrhhapp/src/main/java/com/sistema/rrhh/rrhhapp/controllers/EmpleadoController.java
+++ rrhhapp/src/main/java/com/sistema/rrhh/rrhhapp/controllers/EmpleadoController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/rrhh-app")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 
 public class EmpleadoController {
 
